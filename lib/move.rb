3a7dc32833adeb0afe9415e814f3da1ab4e683fb
Annie Wiegel <annewiegel17@gmail.com>
2021-01-13 01:25:54 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,3 +10,9 @@ end
 def input_to_index(user_input)
   (user_input.to_i)-1
 end
+
+def move(array, index, value="X")
+  def update_array_at_with(array, index, value)
+    array[index] = value
+  end
+end
